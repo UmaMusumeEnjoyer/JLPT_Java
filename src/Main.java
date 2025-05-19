746e4ff91c6018c294030d40575edd4b17a48e45
dungbaoviec123 <dungtran160820051508@gmail.com>
2025-05-19 15:49:49 +0700

    Test DbConnect

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
-
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hoàn thành nhận dạng văn bản tiếng Nhật từ ảnh.");
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/JLPT", "root", "16082005");
+            if (connection != null) {
+                System.out.println("Kết nối thành công!");
+            } else {
+                System.out.println("Kết nối thất bại!");
+            }
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Exams");
+            while (rs.next()) {
+                System.out.println(rs.getString("ExamID") + " " + rs.getString("Title"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
     }
 }
