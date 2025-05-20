f7bbd3c798472c4dfdd6d95ce924c5f73e50cbab
dungbaoviec123 <dungtran160820051508@gmail.com>
2025-05-20 14:43:19 +0700

    Itsumo Kono Basho de

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Xuất ra file PDF ngay trong thư mục project
-            ExportQuestionsPDF.exportToPDF("questions.pdf");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            System.out.println("Lỗi khi xuất PDF: " + ex.getMessage());
-        }
+
     }
 }
